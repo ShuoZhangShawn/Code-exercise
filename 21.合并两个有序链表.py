f34e4191f0f325c166bc4479e2ed5718936e5d93
ShuoZhangShawn <zhangshuoacca@gmail.com>
2025-07-06 20:57:59 +0800

    feat: 完成 LeetCode 21.合并两个有序链表

---
+++ 21.合并两个有序链表.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode.cn id=21 lang=python3
+# @lcpr version=30201
+#
+# [21] 合并两个有序链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [1,2,4]\n[1,3,4]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# []\n[]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# []\n[0]\n
+# @lcpr case=end
+
+#
+
