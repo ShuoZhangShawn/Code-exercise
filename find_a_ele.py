e632d7550a6715cb7e3620f35d6769300316ce4d
Shawn <zhangshuoacca@gmail>
2025-08-19 13:40:00 +0800

    二叉树查找一个元素

--- find_a_ele.py
+++ find_a_ele.py
@@ -1,3 +1,22 @@
 #给你输入一棵没有重复元素的二叉树根节点 root 和一个目标值 val，请你写一个函数寻找树中值为 val 的节点。
 from typing import Optional
 def find(root: TreeNode, val: int) -> TreeNode:
+    #base case
+    if not root:
+        return None
+    #check whether the root node is target
+    if root.val == val:
+        return root
+    #root not the target, go to check the left tree
+    left = find(root.left,val)
+    if left:
+        return left
+    
+    # if not left, check the right
+    right = find(root.right.val)
+    if right:
+        return right
+    
+    #if we find nothing
+    return None
+    
---
+++ peizhi.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from typing import *
+from common.node import *
+
+${question.code}
+
+if __name__ == '__main__':
+    solution = Solution()
+    \# your test code here
